464b98e5f5f7004463a5162497d0a88db1a428b3
Gia Bảo Huỳnh <baohgse182507@fpt.edu.vn>
2025-06-10 10:26:59 +0700

    Revert "Add"
    This reverts commit 6883e26e74c5583a9dc90cc3cb95aadaa4bbf5f1.

--- CareNestAPI/Program.cs
+++ CareNestAPI/Program.cs
@@ -1,8 +1,6 @@
 using System.Configuration;
 using System.Text;
 using CareNestAPI;
-using CareNestAPI.Middleware;
-using CareNestAPI.Filters;
 using DAL;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -13,25 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers(options =>
-{
-    options.Filters.Add<CorsActionFilter>();
-    options.Filters.Add<CorsExceptionFilter>();
-});
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
-// Add CORS configuration
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    });
-});
-
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo { Title = "CarePet API", Version = "v1" });
@@ -89,13 +71,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-// Use custom CORS middleware
-app.UseMiddleware<CorsMiddleware>();
-
-// Enable CORS
-app.UseCors("AllowAll");
-
 app.UseAuthentication();
 app.UseAuthorization();
 
