842182e3f356b3f0ae73b9075baf17afecfbfd7f
Đông Nghi <hoangvodonghi@gmail.com>
2025-06-16 16:53:50 +0700

    Update AccountResponse.cs

--- BOL/DTOs/AccountResponse.cs
+++ BOL/DTOs/AccountResponse.cs
@@ -11,6 +11,7 @@ namespace BOL.DTOs
 {
     public class AccountResponse
     {
+        public string Id { get; set; }
         public string username { get; set; }
         public string password { get; set; }
         public string email { get; set; }
