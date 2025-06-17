79830297bae6131d2bc3fcbb39ea0f76007280a9
TuanAnh <nguyenngoctuananh3001@gmail.com>
2025-06-17 08:13:26 +0700

    add-migration

---
+++ DAL/Migrations/20250617011227_payment.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class payment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BANK_ACCOUNT_NAME",
+                table: "Account",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BANK_ACCOUNT_NO",
+                table: "Account",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BANK_ID",
+                table: "Account",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BANK_ACCOUNT_NAME",
+                table: "Account");
+
+            migrationBuilder.DropColumn(
+                name: "BANK_ACCOUNT_NO",
+                table: "Account");
+
+            migrationBuilder.DropColumn(
+                name: "BANK_ID",
+                table: "Account");
+        }
+    }
+}
